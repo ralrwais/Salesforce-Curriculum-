cf04382268d2144e68ac313c4f57714ad5b001bc
Rima Alrwais <ralrwais@ralrwais-ltm2.internal.salesforce.com>
2017-12-07 11:53:30 -0800

    JS Work

--- Week-1/Javascript/Substring.js
+++ Week-1/Javascript/Substring.js
@@ -3,3 +3,25 @@ let isSubstring = (subStr, str) => {
 	return str.includes(subStr);
 }
 
+
+
+function isSubstring(subStr, str) { 
+	return str.indexOf(subStr) != -1;
+};
+
+
+
+let isSubstring = (sub, str) => {
+	var newStr = '';
+	for(var i = 0; i < str.length; i++){
+		for(var j = 0; j < sub.length; j++){
+			if(str[i] === sub[j]){
+				newStr += sub[j];
+			}
+		}
+	}
+	if(newStr === sub){
+		return true;
+	}
+	return false;
+}
