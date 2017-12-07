526c17ea6cbf2d504684f7b53b19d0b19abbe979
Rima Alrwais <ralrwais@ralrwais-ltm2.internal.salesforce.com>
2017-12-07 10:11:00 -0800

    JS wor

---
+++ Week-1/Javascript/Uniq.js
@@ -0,0 +1,11 @@
+var arr = [1,1,2,2,3,4,5];
+
+let myUniq = (arr) => {
+	let uniqArr = [];
+	for(var i = 0; i < arr.length; i++){
+		if(!uniqArr.includes(arr[i])){
+			uniqArr.push(arr[i]);
+		}
+	}
+	return uniqArr;
+}
